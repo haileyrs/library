e26ed9f38550da4b05dba45ce90f459e05d2b56a
haileyrs <haileyscheevel@gmail.com>
2023-04-27 12:19:07 -0500

    create modal

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 let myLibrary = [];
 
-function Book() {
-
+function Book(title, author, description, pages, read) {
+  this.title = title
+  this.author = author
+  this.description = description
+  this.pages = pages
+  this.read = read
 }
 
-function addBookToLibrary() {
-    
+function addBookToLibrary(e) {
+  newBook = new Book(title, author, description, pages, read)
+  myLibrary.push(newBook)
 }
+
+
+let modal = document.getElementById('modal');
+let addBtn = document.getElementById('addBtn');
+let closeBtn = document.querySelector('.close');
+let saveBtn = document.querySelector('.save');
+
+addBtn.addEventListener('click', () => modal.style.display = "block")
+closeBtn.addEventListener('click', () => modal.style.display = "none")
+saveBtn.addEventListener('click', () => addBookToLibrary(e))
